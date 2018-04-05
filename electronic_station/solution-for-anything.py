e86c8fb23c21cdbf90fe3ed59b9fbaf2799e1c2f
yynozk <yynozk@gmail.com>
2018-04-05 11:03:27 +0900

    Solution for anything

---
+++ electronic_station/solution-for-anything.py
@@ -0,0 +1,10 @@
+class SolutionForAnything:
+    def __lt__(self, other): return True
+    def __le__(self, other): return True
+    def __eq__(self, other): return True
+    def __ne__(self, other): return True
+    def __gt__(self, other): return True
+    def __ge__(self, other): return True
+
+def checkio(anything):
+    return SolutionForAnything()
